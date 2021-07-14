166967bc8882618128c176016c001cdcaab8859c
liyang <484398584@qq.com>
2021-07-14 19:56:14 +0800

    新增类别

--- src/views/modules/shop/goodscategory-add-or-update.vue
+++ src/views/modules/shop/goodscategory-add-or-update.vue
@@ -1,23 +1,17 @@
 <template>
   <el-dialog
-    :title="!dataForm.catId ? '新增' : '修改'"
+    :title="!dataForm.catTwoId ? '新增' : '修改'"
     :close-on-click-modal="false"
     :visible.sync="visible">
     <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="80px">
-    <el-form-item label="联系商品表" prop="catOneId">
-      <el-input v-model="dataForm.catOneId" placeholder="联系商品表"></el-input>
-    </el-form-item>
-    <el-form-item label="级别ID" prop="levelId">
-      <el-input v-model="dataForm.levelId" placeholder="级别ID"></el-input>
+    <el-form-item label="上级ID" prop="parentId">
+      <el-input v-model="dataForm.parentId" placeholder="上级ID"></el-input>
     </el-form-item>
     <el-form-item label="类别名称" prop="catName">
       <el-input v-model="dataForm.catName" placeholder="类别名称"></el-input>
     </el-form-item>
     <el-form-item label="类别图标" prop="catLogo">
       <el-input v-model="dataForm.catLogo" placeholder="类别图标"></el-input>
-    </el-form-item>
-    <el-form-item label="商品数量" prop="catGoodsNumber">
-      <el-input v-model="dataForm.catGoodsNumber" placeholder="商品数量"></el-input>
     </el-form-item>
     </el-form>
     <span slot="footer" class="dialog-footer">
@@ -33,50 +27,40 @@
       return {
         visible: false,
         dataForm: {
-          catId: 0,
-          catOneId: '',
-          levelId: '',
+          catTwoId: 0,
+          parentId: '',
           catName: '',
-          catLogo: '',
-          catGoodsNumber: ''
+          catLogo: ''
         },
         dataRule: {
-          catOneId: [
-            { required: true, message: '联系商品表不能为空', trigger: 'blur' }
-          ],
-          levelId: [
-            { required: true, message: '级别ID不能为空', trigger: 'blur' }
+          parentId: [
+            { required: true, message: '上级ID不能为空', trigger: 'blur' }
           ],
           catName: [
             { required: true, message: '类别名称不能为空', trigger: 'blur' }
           ],
           catLogo: [
             { required: true, message: '类别图标不能为空', trigger: 'blur' }
-          ],
-          catGoodsNumber: [
-            { required: true, message: '商品数量不能为空', trigger: 'blur' }
           ]
         }
       }
     },
     methods: {
       init (id) {
-        this.dataForm.catId = id || 0
+        this.dataForm.catTwoId = id || 0
         this.visible = true
         this.$nextTick(() => {
           this.$refs['dataForm'].resetFields()
-          if (this.dataForm.catId) {
+          if (this.dataForm.catTwoId) {
             this.$http({
-              url: this.$http.adornUrl(`/shop/goodscategory/info/${this.dataForm.catId}`),
+              url: this.$http.adornUrl(`/shop/goodscategory/info/${this.dataForm.catTwoId}`),
               method: 'get',
               params: this.$http.adornParams()
             }).then(({data}) => {
               if (data && data.code === 0) {
-                this.dataForm.catOneId = data.goodsCategory.catOneId
-                this.dataForm.levelId = data.goodsCategory.levelId
+                this.dataForm.parentId = data.goodsCategory.parentId
                 this.dataForm.catName = data.goodsCategory.catName
                 this.dataForm.catLogo = data.goodsCategory.catLogo
-                this.dataForm.catGoodsNumber = data.goodsCategory.catGoodsNumber
               }
             })
           }
@@ -87,15 +71,13 @@
         this.$refs['dataForm'].validate((valid) => {
           if (valid) {
             this.$http({
-              url: this.$http.adornUrl(`/shop/goodscategory/${!this.dataForm.catId ? 'save' : 'update'}`),
+              url: this.$http.adornUrl(`/shop/goodscategory/${!this.dataForm.catTwoId ? 'save' : 'update'}`),
               method: 'post',
               data: this.$http.adornData({
-                'catId': this.dataForm.catId || undefined,
-                'catOneId': this.dataForm.catOneId,
-                'levelId': this.dataForm.levelId,
+                'catTwoId': this.dataForm.catTwoId || undefined,
+                'parentId': this.dataForm.parentId,
                 'catName': this.dataForm.catName,
-                'catLogo': this.dataForm.catLogo,
-                'catGoodsNumber': this.dataForm.catGoodsNumber
+                'catLogo': this.dataForm.catLogo
               })
             }).then(({data}) => {
               if (data && data.code === 0) {
